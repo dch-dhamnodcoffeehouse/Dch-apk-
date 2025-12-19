436ffa7d20222fc4e42bc9f06b654464a462366d
Dhamnod coffee house <coffeehousedhamnod@gmail.com>
2025-12-20 02:33:50 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'dch-app-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
